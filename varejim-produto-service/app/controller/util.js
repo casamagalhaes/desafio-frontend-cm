89276790f8e33fd1e6cafd9eaf1da4bae077a1c4
Leonardo Braga <leonardoctbraga@gmail.com>
2021-03-04 12:26:21 -0300

    Ajuste no controller

--- varejim-produto-service/app/controller/util.js
+++ varejim-produto-service/app/controller/util.js
@@ -1,6 +1,7 @@
 export const trata = async (req, res, fn, ...param) => {
   try {
-    res.send(await fn(...param));
+    let response = await fn(...param);
+    res.send(JSON.stringify(response));
   } catch (err) {
     res.status(500).send(err);
     console.log(err);
